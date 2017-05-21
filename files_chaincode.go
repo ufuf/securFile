59d335de587cda6fbadadc4727baadc3327e8772
Ufuk ARSLAN <ufuk@mobilist.com.tr>
2017-05-21 16:07:02 +0300

    initial commit

---
+++ files_chaincode.go
@@ -0,0 +1,252 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+var fileIndexStr = "_fileindex" //name for the key/value that will store a list of all known files
+
+type File struct {
+	Name       string `json:"name"` //the fieldtags are needed to keep case from bouncing around
+	Hash       string `json:"hash"`
+	Properties int    `json:"properties"`
+	User       string `json:"user"`
+}
+
+type Description struct {
+	Hash       string `json:"hash"`
+	Properties int    `json:"properties"`
+}
+
+// ============================================================================================================================
+// Main
+// ============================================================================================================================
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+// ============================================================================================================================
+// Init - reset all the things
+// ============================================================================================================================
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var Aval int
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	// Initialize the chaincode
+	Aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+
+	// Write the state to the ledger
+	err = stub.PutState("ufukInitTest", []byte(strconv.Itoa(Aval))) //making a test var "abc", I find it handy to read/write to it right away to test the network
+	if err != nil {
+		return nil, err
+	}
+
+	var empty []string
+	jsonAsBytes, _ := json.Marshal(empty) //marshal an emtpy array of strings to clear the index
+	err = stub.PutState(fileIndexStr, jsonAsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Run - Our entry point for Invocations - [LEGACY] obc-peer 4/25/2016
+// ============================================================================================================================
+func (t *SimpleChaincode) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("run is running " + function)
+	return t.Invoke(stub, function, args)
+}
+
+// ============================================================================================================================
+// Invoke - Our entry point for Invocations
+// ============================================================================================================================
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "init" { //initialize the chaincode state, used as reset
+		return t.Init(stub, "init", args)
+	} else if function == "write" { //writes a value to the chaincode state
+		return t.Write(stub, args)
+	} else if function == "init_file" { //create a new file
+		return t.init_file(stub, args)
+	}
+	fmt.Println("invoke did not find func: " + function) //error
+
+	return nil, errors.New("Received unknown function invocation")
+}
+
+// ============================================================================================================================
+// Query - Our entry point for Queries
+// ============================================================================================================================
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function) //error
+
+	return nil, errors.New("Received unknown function query")
+}
+
+// ============================================================================================================================
+// Read - read a variable from chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
+	}
+
+	name = args[0]
+	valAsbytes, err := stub.GetState(name) //get the var from chaincode state
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil //send it onward
+}
+
+// ============================================================================================================================
+// Write - write variable into chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) Write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var name, value string // Entities
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
+	}
+
+	name = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(name, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Init File - create a new file, store into chaincode state
+// ============================================================================================================================
+func (t *SimpleChaincode) init_file(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+
+	//   0       1       2     3
+	// "asdf", "blue", "35", "bob"
+	if len(args) != 4 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	}
+
+	//input sanitation
+	fmt.Println("- start init file")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return nil, errors.New("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
+	name := args[0]
+	hash := strings.ToLower(args[1])
+	user := strings.ToLower(args[3])
+	properties := strings.ToLower(args[2])
+
+	//check if file already exists
+	fileAsBytes, err := stub.GetState(hash)
+	if err != nil {
+		return nil, errors.New("Failed to get file hash")
+	}
+	res := File{}
+	json.Unmarshal(fileAsBytes, &res)
+	if res.Hash == hash {
+		fmt.Println("This file arleady exists: " + hash)
+		fmt.Println(res)
+		return nil, errors.New("This file arleady exists") //all stop a file by this name exists
+	}
+
+	//build the file json string manually
+	str := `{"name": "` + name + `", "hash": "` + hash + `", "properties": ` + properties + `, "user": "` + user + `"}`
+	err = stub.PutState(hash, []byte(str)) //store file with id as key
+	if err != nil {
+		return nil, err
+	}
+
+	//get the file index
+	filesAsBytes, err := stub.GetState(fileIndexStr)
+	if err != nil {
+		return nil, errors.New("Failed to get file index")
+	}
+	var fileIndex []string
+	json.Unmarshal(filesAsBytes, &fileIndex) //un stringify it aka JSON.parse()
+
+	//append
+	fileIndex = append(fileIndex, hash) //add file hash to index list
+	fmt.Println("! file index: ", fileIndex)
+	jsonAsBytes, _ := json.Marshal(fileIndex)
+	err = stub.PutState(fileIndexStr, jsonAsBytes) //store hash of file
+
+	fmt.Println("- end init file")
+	return nil, nil
+}
+
+// ============================================================================================================================
+// Make Timestamp - create a timestamp in ms
+// ============================================================================================================================
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / (int64(time.Millisecond) / int64(time.Nanosecond))
+}
